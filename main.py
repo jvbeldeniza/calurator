e76281edb4d58bfe7d9ba1ef5b08d0cc0dc259fa
jvbeldeniza <153980026+jvbeldeniza@users.noreply.github.com>
2025-05-18 10:55:46 +0800

    CHange

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import plotly.graph_objects as go
 
 st.set_page_config(page_title="Food Tracker", page_icon="🍽️")
 
+
 st.title("🍽️ Welcome to the Food Tracker App")
 st.markdown("Use the sidebar to navigate between Meal Scan & Log Food")
 
